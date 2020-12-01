c458e164692e0df792c7f65a3f7fa3d0c5913b06
ilya.smiyukha <ilya.smiyuha@gmail.com>
2020-12-01 20:55:19 +0200

    add config file

---
+++ src/config/ConfigExample.tsx
@@ -0,0 +1,5 @@
+//Token - github token. How create it check here https://docs.github.com/en/free-pro-team@latest/github/authenticating-to-github/creating-a-personal-access-token
+
+export const config = {
+  token: ''
+}
