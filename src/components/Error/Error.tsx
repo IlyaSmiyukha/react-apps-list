e54338a4afbd25971408351fff2be50f11d7152e
ilya.smiyukha <ilya.smiyukha@gmail.com>
2020-12-02 22:13:40 +0200

    fix typo

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Error.css';
 
 const Error = (): JSX.Element => {
-  return <h1>Oooops Something went wtong!</h1>;
+  return <h1>Oooops Something went wrong!</h1>;
 }
 
 export default Error;
